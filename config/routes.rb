747090e26d6af9bd34d09843afb710ac8293c4e7
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 10:45:37 -0800

    Edit routes to include users and session

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   get('/', { to: 'ideas#index', as: 'ideas_path' })
 
-  
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end
