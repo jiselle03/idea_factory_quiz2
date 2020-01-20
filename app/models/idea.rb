1929fc4e878d110fc2c3093a00f1b86d765887b7
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 11:12:29 -0800

    Add associations, add validations for user

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,7 @@
 class Idea < ApplicationRecord
+    belongs_to :user
+    has_many :reviews
+    
     validates :title, presence: true, uniqueness: { case_sensitive: false }
     validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :ideas, dependent: :nullify
+    has_many :reviews, dependent: :nullify
+
+    validates :email, presence: true, uniqueness: true,
+    format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    has_secure_password
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
