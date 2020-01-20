6459ed838f5b6e220571320b33c27a0e10459bdf
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 11:56:49 -0800

    Add routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ideas do
     resources :reviews
+    resources :likes, shallow: true, only: [:create, :destroy]
   end
 
   get('/', { to: 'ideas#index', as: 'root' })
