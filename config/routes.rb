4ea66f0ea7811c1cdbfcabc804bfa26e44792752
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 10:44:00 -0800

    Add routes for users and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :ideas
+  resources :ideas do
+    resources :reviews
+  end
 
   get('/', { to: 'ideas#index', as: 'ideas_path' })
+
+  
 end
