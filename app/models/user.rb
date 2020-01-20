2a033087ccadc658d8009975e7bcae785890afbc
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 12:33:36 -0800

    Add association between user and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :ideas, dependent: :nullify
     has_many :reviews, dependent: :nullify
+    has_many :likes, dependent: :destroy
 
     validates :email, presence: true, uniqueness: true,
     format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
