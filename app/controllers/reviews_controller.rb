60ea50e52ea6a96a271f8375dbe858a0a05bdab9
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 13:02:41 -0800

    Add create and destroy for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @idea = Idea.find params[:idea_id]
+        @review = Review.new review_params
+        @review.user = current_user
+        @review.idea = @idea
+
+        if @review.save
+            redirect_to idea_path(@idea)
+        else
+            @reviews = @idea.reviews.order(created_at: :desc)
+            render 'ideas/show'
+        end
+    end
+
+    def destroy
+        @review = Review.find params[:id]
+        if can? :crud, @review
+            @review.destroy
+            redirect_to idea_path(@review.idea)
+        else
+            flash[:danger] = "Access denied"
+            redirect_to idea_path(@review.idea)
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:body)
+    end
 end
