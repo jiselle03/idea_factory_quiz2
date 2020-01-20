9393a6c3be612adfdc42664c3df4b88a2af6d3cc
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 11:57:31 -0800

    Add validations to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :idea
   belongs_to :user
+
+  validates(
+    :idea_id, 
+    uniqueness: {
+      scope: :user_id,
+      message: "You have already liked this."
+    }
+  )
 end
