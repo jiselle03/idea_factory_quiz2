65b1e097af48d5a651f0213ee2552bc0386d7e6d
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 14:52:19 -0800

    Assign user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    user ||= User.new
+    
     alias_action :create, :read, :update, :destroy, to: :crud
 
     if user.is_admin?
