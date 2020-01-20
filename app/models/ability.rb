5986c2a893f4d40390dc501454ba8c1383e8a2cb
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 11:25:50 -0800

    Add ability file and include authorizations for idea and review and for admin user

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    alias_action :create, :read, :update, :destroy, to: :crud
+
+    if user.is_admin?
+      can :manage, :all
+    end
+    
+    can(:crud, Idea) do |idea|
+      idea.user == user 
+    end
+
+    can(:crud, Review) do |review|
+      review.user == user || review.idea.user == user
+    end
+
+  end
+end
