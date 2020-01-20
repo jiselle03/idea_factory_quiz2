58bf1152684af75d1a6566958e773040a16247e0
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 11:55:28 -0800

    Add create and destroy to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,30 @@
 class LikesController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        idea = Idea.find params[:idea_id]
+        like = Like.new(user: current_user, question: idea)
+        if can? :like, @idea
+            if like.save
+                flash[:success] = "Idea liked"
+            else
+                flash[:danger] = like.errors.full_messages.join(", ")
+            end
+            redirect_to @idea
+        else
+            flash[:danger] = "You can't like your own idea!" 
+            redirect_to idea_path(@idea)
+        end
+    end
+
+    def destroy
+        like = current_user.likes.find params[:id]
+        if can? :destroy, like
+            like.destroy
+            flash[:success] = "Idea unliked"
+            redirect_to idea_path(like.idea)
+        else
+            flash[:alert] = "Can't delete like!"
+        end
+    end
 end
