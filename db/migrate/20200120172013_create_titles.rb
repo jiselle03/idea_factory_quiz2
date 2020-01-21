7065a353ae632eb95a7124942e5968349c566e12
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 19:34:23 -0800

    Remove unnecessary table model and migration

--- db/migrate/20200120172013_create_titles.rb
+++
@@ -1,9 +0,0 @@
-class CreateTitles < ActiveRecord::Migration[6.0]
-  def change
-    create_table :titles do |t|
-      t.text :description
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -43,12 +43,6 @@ ActiveRecord::Schema.define(version: 2020_01_20_194657) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
