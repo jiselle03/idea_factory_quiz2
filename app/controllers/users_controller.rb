c62038c0412c9c19242e1995c0335e940ac7a3b3
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 18:01:22 -0800

    Add authorize! for edit and update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: [:edit, :update]
     before_action :find_user, only: [:edit, :update, :edit_password, :update_password]
-    before_action :authorize!, only: [:edit, :update]
-    
+    before_action :authorize!, only: [:edit, :update, :edit_password, :update_password]
+
     def new
         @user = User.new
     end
